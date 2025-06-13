70df7cc38a18446510d4931eee048b330f713ef5
CleuJunior <cleu.junior@gmail.com>
2025-06-13 05:42:57 -0300

    feat: Date and time converter and audit

---
+++ supermarket-delivery-products/src/main/java/br/com/smd/products/supermarket_delivery_products/config/MongoConfigurations.java
@@ -0,0 +1,24 @@
+package br.com.smd.products.supermarket_delivery_products.config;
+
+import br.com.smd.products.supermarket_delivery_products.converter.ZonedDateTimeReadConverter;
+import br.com.smd.products.supermarket_delivery_products.converter.ZonedDateTimeWriteConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableMongoAuditing(dateTimeProviderRef = "dateTimeProvider")
+@Configuration
+public class MongoConfigurations {
+    @Bean
+    public MongoCustomConversions customConversions() {
+        List<Converter<?, ?>> converters = new ArrayList<>();
+        converters.add(new ZonedDateTimeReadConverter());
+        converters.add(new ZonedDateTimeWriteConverter());
+        return new MongoCustomConversions(converters);
+    }
+}
