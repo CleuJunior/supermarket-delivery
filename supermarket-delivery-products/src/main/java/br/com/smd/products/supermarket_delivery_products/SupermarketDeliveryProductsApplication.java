5f96df2f48c30f85d4aab778273757250846f12c
CleuJunior <cleu.junior@gmail.com>
2025-06-13 05:12:08 -0300

    feat: initial project

---
+++ supermarket-delivery-products/src/main/java/br/com/smd/products/supermarket_delivery_products/SupermarketDeliveryProductsApplication.java
@@ -0,0 +1,13 @@
+package br.com.smd.products.supermarket_delivery_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketDeliveryProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketDeliveryProductsApplication.class, args);
+	}
+
+}
---
+++ supermarket-delivery-products/src/test/java/br/com/smd/products/supermarket_delivery_products/SupermarketDeliveryProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smd.products.supermarket_delivery_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketDeliveryProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
